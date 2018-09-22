7f3a0c8cfacf27045c5895d226436bed6f8f5c2e
Luis Gustavo <gustakitos@Gustavos-Macbook.local>
2018-09-22 15:36:30 -0300

    first commit

---
+++ TravelRecordApp/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TravelRecordApp {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
+            InitializeComponent();
+        }
+
+        public void Login_Clicked(object sender, System.EventArgs e) {
+            bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
+
+            if(isEmailEmpty || isPasswordEmpty) {
+
+            }
+            else {
+                Navigation.PushAsync(new HomePage());
+            }
+        }
+
+    }
+}
